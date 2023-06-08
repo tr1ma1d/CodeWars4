d46467da67155340f2f6ff81395645811f3679e1
Никита Мусатов <la3xmus@gmail.com>
2023-06-08 14:08:05 +0300

    Добавьте файлы проекта.

---
+++ CodeWars4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars4
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var sheeps = new bool[] { true, false, true };
+            int count = 0;
+            for(int i = 0; i < sheeps.Length; i++)
+            {
+                if (sheeps[i] == true) {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
